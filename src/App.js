edf7468645a340a4ab1ad0ab7f471b0340c3c051
Rania <raniayaqoubi1995@gmail.com>
2024-06-10 10:35:06 +0200

    changing link title

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello world test
+          Bonjour
         </a>
       </header>
     </div>
